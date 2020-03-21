1690bfc2d1947e9383c61bea80a97c130b34a5bb
lennartoe <48726910+lennartoe@users.noreply.github.com>
2020-03-21 19:58:39 +0100

    Lennart Timeline

--- src/pages/history.vue
+++ src/pages/history.vue
@@ -1,8 +1,29 @@
 <template>
  <v-app>
-     <v-btn @click="fetch">fetch</v-btn>
+     <v-timeline align-top dense>
+     <v-timeline-item v-for="elements in Anzeigen" :key="elements.name" 
+     color="green" 
+     > 
+         <v-row class="1">
+             <v-col cols="3">
+                 <v-expansion-panels>
+                 <v-expansion-panel flat>
+                 <v-expansion-panel-header> <strong> {{elements.name}}</strong></v-expansion-panel-header>
+                 <v-expansion-panel-content>
+                     {{elements.Content}}
+                 </v-expansion-panel-content>
+                 </v-expansion-panel>
+                 </v-expansion-panels>
+             </v-col>
+        </v-row>
+     </v-timeline-item>
+     
+    </v-timeline>
 </v-app> 
+
+
 </template>
+
 
 <script>
 import * as firebase from "firebase/app";
@@ -11,8 +32,18 @@ import "firebase/firestore";
 export default {
     data() {
         return {
-            Anzeigen: [],
+            Anzeigen: [{
+                name: "Thomas",
+                Ort: "Bonn",
+                Content: "Hallo Welt hier sind infos"
+            },
+            {
+                name: "Martin",
+                Ort: "Hamburg",
+                Content: "Hallo Welt hier sind infos"
+            }],
             AnzeigenID: [],
+            
         }
     },
     methods: {
