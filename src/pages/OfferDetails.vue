7ebdb41792041d1d4f991c3976382d4de9d4e2d6
Fabian Metschies <fabian.metschies@gmail.com>
2020-03-21 20:28:21 +0100

    update offer details

--- src/pages/OfferDetails.vue
+++ src/pages/OfferDetails.vue
@@ -2,8 +2,63 @@
   <div class="offer-details" v-if="offer && agrarian">
     <div class="inner">
       <h1>{{ offer.title }}</h1>
-      <h2>{{ agrarian.name }}</h2>
-      <p>{{ offer }}</p>
+      <div class="status-chip">
+        Status:
+        {{
+          offer.maxHelpers -
+            offer.helperCount +
+            "/" +
+            offer.maxHelpers +
+            " Helfern fehlen noch"
+        }}
+      </div>
+
+      <div class="description">
+        <div class="description-header section-header">
+          Beschreibung
+        </div>
+        <div class="description-body section-body">
+          {{ offer.description }}
+        </div>
+      </div>
+
+      <div class="equipment">
+          <div class="equipment-header section-header">
+              Benötigtes Equipment
+          </div>
+          <div class="equipment-body section-body">
+              {{offer.equipment.join(', ')}}
+          </div>
+      </div>
+
+      <div class="section-header">Starttermine</div>
+      <p><b>Hinweis:</b> Die Mindestdauer der Arbeit beträgt <b>{{offer.minDuration}} Tage</b></p>
+      <v-list>
+        <v-card
+          v-for="startDate in offer.startDates"
+          :key="startDate.seconds"
+          class="date-list-item"
+        >
+          <v-list-item>
+            <div class="list-date">{{ new Date(startDate.seconds) }}</div>
+          </v-list-item>
+        </v-card>
+      </v-list>
+      <h2>Kontakt</h2>
+      <div class="contact-box">
+        <div class="address">
+          {{ agrarian.name }}
+          <br />
+          {{ agrarian.address.street + " " + agrarian.address.number }}
+          <br />
+          {{ agrarian.address.postcode + " " + agrarian.address.city }}
+        </div>
+        <div class="email">
+          <a :href="'mailto:' + agrarian.publicEmail">{{
+            agrarian.publicEmail
+          }}</a>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -28,7 +83,7 @@ export default {
       .then(snapshot => {
         if (snapshot.exists) {
           this.offer = { ...snapshot.data(), id: snapshot.id };
-          console.log(snapshot.data());
+          console.log(snapshot.data().startDates);
           return firestore.doc("agrarians/" + snapshot.data().agrarianId).get();
         } else {
           throw snapshot;
@@ -42,12 +97,34 @@ export default {
       .catch(err => {
         console.log(err);
       });
-  }
+  },
+  filters: {}
 };
 </script>
-<style scoped lang="scss">
-.inner {
-    width: 95vw;
-    max-width: 1200px;
+<style lang="scss" scoped>
+h1,
+h2 {
+  font-weight: 300;
+}
+.contact-box {
+  display: flex;
+  flex-direction: column;
+}
+.contact-card {
+  padding: 8px;
+}
+.status-chip {
+  padding: 8px;
+  border: 1px solid red;
+  border-radius: 5px;
+  display: inline-block;
+}
+.date-list-item {
+  margin-bottom: 8px;
+}
+.section-header {
+    font-size: 24px;
+    font-weight: 300;
+    margin: 8px 0;
 }
 </style>
