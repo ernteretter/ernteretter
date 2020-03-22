76536b87f3aa7e62e4ca3101835e6e39b2277ef8
remunds <raban.emunds@gmail.com>
2020-03-22 15:54:56 +0100

    theme

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,7 +1,22 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify/lib';
 
-Vue.use(Vuetify);
+const vuetify = new Vuetify({
+    theme: {
+        themes: {
+            light: {
+                primary: "#ed9a00",
+                secondary: '#4d4238',
+                gelb: "#ffdf59",
+                dunkelgrau: "#282828",
+                schwarz: "#000000",
+                hellgrau: "#a0a0a0"
+            }
+        }
+    }
+})
+
+Vue.use(vuetify);
 
 export default new Vuetify({
 });
