58ff4f7b34a5a33c1dda9dcc790e91f630ae0173
wmerdes <w.merdes@hiorg-server.de>
2020-03-22 16:28:15 +0100

    register Helper: layout corrections

--- src/pages/registerHelper.vue
+++ src/pages/registerHelper.vue
@@ -1,136 +1,149 @@
 <template>
     <div class="registerHelper">
-        <h1>Erstelle deinen Profil!</h1>
-        <v-card>
-            <h3>
-                Wie heißt du?
-            </h3>
-            <v-col cols="5" sm="5">
-                <v-text-field v-model="name" label="Name" single-line solo></v-text-field>
-            </v-col>
-        </v-card>
 
-        <v-card>
-            <h3>
-                Wähle ein Passwort
-            </h3>
-            <v-col cols="5" sm="5">
-                <v-text-field
-                        :type="showPassword ? 'text' : 'password'"
-                        label="Passwort" :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
-                        @click:append="showPassword = !showPassword"
-                        v-model="password"
-                        single-line
-                        solo
-                />
-            </v-col>
-        </v-card>
-
-        <v-card>
-            <h3>
-                Wie ist deine E-Mail Adresse?
-            </h3>
-            <v-col cols="5" sm="5">
-                <v-text-field v-model="mail" label="E-Mail" single-line solo></v-text-field>
-            </v-col>
-        </v-card>
-
-        <v-card>
-            <h3> Für welche Arten der Ernte interessierst du dich? </h3>
-            <v-col cols="5" sm="5">
-                <v-checkbox
-                        class="mt-1"
-                        v-for="item in harvestTypesOptions"
-                        v-bind:key="item"
-                        v-model="harvestTypes"
-                        :label="item"
-                        :value="item"
-                >
-                </v-checkbox>
-            </v-col>
-        </v-card>
-
-        <v-card>
-            <h3> In welchem Umkreis möchtest du Hilfemöglichkeiten vorgschlagen bekommen? </h3>
-            <v-col cols="5" sm="5">
-
-                <v-slider
-                        v-model="searchRange"
-                        class="align-center"
-                        :max="searchRangeMax"
-                        :min="searchRangeMin"
-                        hide-details
-                >
-                    <template v-slot:append>
-                        <v-text-field
-                                v-model="searchRange"
-                                class="mt-0 pt-0"
-                                hide-details
-                                single-line
-                                type="number"
-                                style="width: 60px"
-                        ></v-text-field>
-                        <v-chip class="ma-2" outlined>
-                            km
-                        </v-chip>
-                    </template>
-                </v-slider>
-            </v-col>
-        </v-card>
-
-        <v-card>
-            <h3> Wie lange möchtest du helfen? </h3>
-            <v-row>
-            <v-col md="1">
-                <v-text-field
-                        class="ml-12"
-                        label="von"
-                        v-model="durationMin"
-                        type="number"
-                        style="width: 30px"
-                >
-                </v-text-field>
-            </v-col>
-            <v-col md="1">
-                <v-select
-                        d-inline-block
-                        v-model="durationMinType"
-                        :items="durationTypeOptions"
-                        style="width: 100px"
-                ></v-select>
-            </v-col>
-            <v-col md="1">
-                <v-text-field
-                        class="ml-12"
-                        label="bis"
-                        v-model="durationMax"
-                        type="number"
-                        style="width: 30px"
-                >
-                </v-text-field>
-            </v-col>
-            <v-col md="1">
-                <v-select
-                        v-model="durationMaxType"
-                        :items="durationTypeOptions"
-                        item-text="text"
-                        item-value="value"
-                        style="width: 100px"
-                ></v-select>
-            </v-col>
+        <v-container fluid>
+            <v-row justify="center">
+                <v-img :src="require('../assets/ernteretter.png')" max-width="400"></v-img>
             </v-row>
-        </v-card>
+            <br/><br/>
 
-        <v-card>
-            <h3> welche Vorerfahrungen hast du? </h3>
-            <v-col cols="5" sm="5">
-                <v-textarea v-model="experience" label="Vorerfahrungen" solo></v-textarea>
-            </v-col>
-        </v-card>
+            <h1 align="center">Erstelle deinen Profil!</h1>
+            <br/>
+            <v-row justify="center">
+                <v-col cols="5" sm="5">
+                    <v-text-field v-model="name" label="Wie heißt du?" single-line solo></v-text-field>
+                </v-col>
+            </v-row>
 
-        <v-btn rounded color="primary" dark @click="registerHelper()">
-            Absenden
-        </v-btn>
+
+            <v-row justify="center">
+                <v-col cols="5" sm="5">
+                    <v-text-field
+                            :type="showPassword ? 'text' : 'password'"
+                            label="Wähle ein Passwort" :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
+                            @click:append="showPassword = !showPassword"
+                            v-model="password"
+                            single-line
+                            solo
+                    />
+                </v-col>
+            </v-row>
+
+            <v-row justify="center">
+                <v-col cols="5" sm="5">
+                    <v-text-field v-model="mail" label="Deine E-Mail-Adresse lautet?" single-line solo></v-text-field>
+                </v-col>
+            </v-row>
+            <br/>
+
+            <v-row justify="center">
+                <div>
+                    <h3> Für welche Arten der Ernte interessierst du dich? </h3>
+                    <v-col cols="5" sm="5">
+                        <v-checkbox
+                                class="mt-1"
+                                v-for="item in harvestTypesOptions"
+                                v-bind:key="item"
+                                v-model="harvestTypes"
+                                :label="item"
+                                :value="item"
+                        >
+                        </v-checkbox>
+                    </v-col>
+                </div>
+            </v-row>
+            <br/>
+
+            <v-row justify="center">
+                <div>
+                    <h3> In welchem Umkreis möchtest du Hilfemöglichkeiten vorgschlagen bekommen? </h3>
+                    <v-col cols="10" sm="12">
+
+                        <v-slider
+                                v-model="searchRange"
+                                class="align-center"
+                                :max="searchRangeMax"
+                                :min="searchRangeMin"
+                                hide-details
+                        >
+                            <template v-slot:append>
+                                <v-text-field
+                                        v-model="searchRange"
+                                        class="mt-0 pt-0"
+                                        hide-details
+                                        single-line
+                                        type="number"
+                                        style="width: 60px"
+                                ></v-text-field>
+                                <v-chip class="ma-2" outlined>
+                                    km
+                                </v-chip>
+                            </template>
+                        </v-slider>
+                    </v-col>
+                </div>
+            </v-row>
+            <br/>
+
+            <v-row justify="center">
+                <div>
+                    <h3> Wie lange möchtest du helfen? </h3>
+                </div>
+            </v-row>
+
+            <v-row justify="center">
+                <v-col md="1">
+                    <v-text-field
+                            class="ml-12"
+                            label="von"
+                            v-model="durationMin"
+                            type="number"
+                            style="width: 30px"
+                    >
+                    </v-text-field>
+                </v-col>
+                <v-col md="1">
+                    <v-select
+                            d-inline-block
+                            v-model="durationMinType"
+                            :items="durationTypeOptions"
+                            style="width: 100px"
+                    ></v-select>
+                </v-col>
+                <v-col md="1">
+                    <v-text-field
+                            class="ml-12"
+                            label="bis"
+                            v-model="durationMax"
+                            type="number"
+                            style="width: 30px"
+                    >
+                    </v-text-field>
+                </v-col>
+                <v-col md="1">
+                    <v-select
+                            v-model="durationMaxType"
+                            :items="durationTypeOptions"
+                            item-text="text"
+                            item-value="value"
+                            style="width: 100px"
+                    ></v-select>
+                </v-col>
+            </v-row>
+            <br/>
+
+            <v-row justify="center">
+                <v-col cols="5" sm="5">
+                    <v-textarea v-model="experience" label="Welche Vorerfahrungen hast du?" solo></v-textarea>
+                </v-col>
+            </v-row>
+
+            <v-row justify="center">
+                <v-btn rounded color="primary" dark @click="registerHelper()">
+                    Absenden
+                </v-btn>
+            </v-row>
+        </v-container>
     </div>
 </template>
 
