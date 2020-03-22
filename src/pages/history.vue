01fef272f61fe75c656e247a4470ef42e7661f03
iBims1JFK <jonathan.kinzel@googlemail.com>
2020-03-22 18:54:17 +0100

    search funktioniert noch nicht ganz

--- src/pages/history.vue
+++ src/pages/history.vue
@@ -1,8 +1,20 @@
 <template>
 <v-app>
     <v-card width="60%" class="mx-auto mt-5">
+        <v-card-title class="primary">
+            <span class="headline white--text">Termine</span>
+            <v-spacer></v-spacer>
+            <v-text-field v-model="search" append-icon="mdi-magnify" label="Search" single-line hide-details></v-text-field>
+            <v-tooltip bottom>
+                <template v-slot:activator="{ on }">
+                    <v-btn v-on="on" @click="$router.push('createOffer')">+</v-btn>
+                </template>
+                <span>neue Anzeige erstellen</span>
+            </v-tooltip>
+        </v-card-title>
+        <v-spacer></v-spacer>
         <v-container>
-            <v-data-table :headers="headers" :items="mixedOffers" item-key="id" :loading="loading" loading-text="Lädt...Bitte warten" :server-items-length="offers.length" class="elevation-1" :search="search">
+            <v-data-table :headers="headers" :items="mixedOffers" item-key="name" :loading="loading" loading-text="Lädt...Bitte warten" :server-items-length="mixedOffers.length" class="elevation-1" :search="search">
                 <template v-slot:item.action="{item}">
                     <v-icon small class="mr-2" @click="editItem(item)" v-if="item.agrarianId == user.uid">mdi-pencil-outline </v-icon>
                     <v-icon small class="mr-2" @click="deleteItem(item)" v-if="item.agrarianId == user.uid">mdi-delete</v-icon>
@@ -11,6 +23,8 @@
             </v-data-table>
         </v-container>
     </v-card>
+    <v-snackbar v-if="deleteSnackSuccess" color="success">Termin erfolgreich gelöscht</v-snackbar>
+    <v-snackbar v-if="deleteSnackFail" color="red">Termin konnte nicht gelöscht werden</v-snackbar>
 </v-app>
 </template>
 
@@ -26,14 +40,46 @@ export default {
             }
         });
     },
+    watch: {
+        search(val) {
+            console.log(val);
+            console.log(this.user.uid);
+            this.mixedOffers = [];
+            firebase.firestore().collection('acceptedOffers').where('helperId', '==', this.user.uid).where('title', "==", val).get().then(async (querySnapshot) => {
+                querySnapshot.forEach(async (doc) => {
+                    var data = doc.data()
+                    this.offerIDs.push(data.offerId)
+                })
+                for (let offerID of this.offerIDs) {
+                    firebase.firestore().collection('offers').doc(offerID).get().then(async (querySnapshot) => {
+                        if (querySnapshot.exists) {
+                            this.mixedOffers.push({
+                                ...querySnapshot.data(),
+                                id: querySnapshot.id
+                            })
+                        }
+                    })
+                }
+            })
+            firebase.firestore().collection('offers').where('agrarianId', '==', this.user.uid).where('title', "==", val).get().then(async (querySnapshot) => {
+                querySnapshot.forEach(async (doc) => {
+                    this.mixedOffers.push({
+                        ...doc.data(),
+                        id: doc.id
+                    })
+                })
+            })
+
+        }
+    },
     data() {
         return {
+            deleteSnackSuccess: true,
+            deleteSnackFail: true,
             user: false,
             isAgrarian: false,
             mixedOffers: [],
-            offers: [],
             offerIDs: [],
-            myOffers: [],
             search: '',
             loading: false,
             headers: [{
@@ -74,41 +120,56 @@ export default {
                 for (let offerID of this.offerIDs) {
                     firebase.firestore().collection('offers').doc(offerID).get().then(async (querySnapshot) => {
                         if (querySnapshot.exists) {
-                            this.mixedOffers.push(querySnapshot.data())
-                            this.offers.push(querySnapshot.data())
+                            this.mixedOffers.push({
+                                ...querySnapshot.data(),
+                                id: querySnapshot.id
+                            })
                         }
                     })
                 }
             })
             firebase.firestore().collection('offers').where('agrarianId', '==', usID).get().then(async (querySnapshot) => {
                 querySnapshot.forEach(async (doc) => {
-                    var data = doc.data()
-                    this.mixedOffers.push(data)
-                    this.myOffers.push(data)
+                    this.mixedOffers.push({
+                        ...doc.data(),
+                        id: doc.id
+                    })
                 })
             })
-            console.log(this.offers);
-            console.log(this.myOffers);
-            console.log(this.mixedOffers);
-           
+
             /*
             this.mixedOffers = this.mixedOffers.sort((a, b) => {
                 a > b ? 1 : (b > a ? -1 : 0)
             }) */
-            
+
         },
         showItem(item) {
-            //TODO
-            const index = this.bills.indexOf(item)
-            console.log('zeige ' + index);
+            this.$router.push("offer/" + item.id)
         },
         editItem(item) {
-            this.$router.push("editOffer/" + item)
+            this.$router.push("editOffer/" + item.id)
 
         },
         deleteItem(item) {
-            //TODO
-            console.log('lösche ' + item);
+            console.log("versuche");
+            try {
+                console.log("mache");
+                console.log(item.id);
+
+                firebase.firestore().collection('offers').doc(item.id).delete().then(() => {
+                    setTimeout(() => (this.deleteSnackSuccess = true), 3000);
+                    this.deleteSnackSuccess = false
+                    this.mixedOffers = this.mixedOffers.filter((a) => {
+                        return a.id != item.id
+                    })
+                    console.log(this.mixedOffers);
+
+                })
+            } catch {
+                setTimeout(() => (this.deleteSnackFail = true), 3000);
+                this.deleteSnackFail = false
+
+            }
         },
     }
 }
