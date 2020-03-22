ab4fdd52fc7f64060b1498aeb3c07e7939a4d3f5
mxsrv <max.saerov@gmail.com>
2020-03-22 14:55:50 +0100

    Cookies added

--- src/App.vue
+++ src/App.vue
@@ -1,65 +1,74 @@
 <template>
-<v-app>
+  <v-app>
     <v-app-bar app>
-        <v-spacer>
-            <v-icon>mdi-barley</v-icon>
-            <router-link to="/">Startseite</router-link>
-            <router-link to="/offers">Anzeigen</router-link>
-            <router-link to="/history">Termine</router-link>
-            <router-link to="/settings">Einstellungen</router-link>
-        </v-spacer>
-        <v-btn color="success" @click="$router.push('/login')" v-if="!user" outlined>login</v-btn>
-        <v-btn color="red" @click="logout()" v-if="user" outlined>log out</v-btn>
+      <v-spacer>
+        <v-icon>mdi-barley</v-icon>
+        <router-link to="/">Startseite</router-link>
+        <router-link to="/offers">Anzeigen</router-link>
+        <router-link to="/history">Termine</router-link>
+        <router-link to="/settings">Einstellungen</router-link>
+      </v-spacer>
+      <v-btn color="success" @click="$router.push('/login')" v-if="!user" outlined>login</v-btn>
+      <v-btn color="red" @click="logout()" v-if="user" outlined>log out</v-btn>
     </v-app-bar>
 
     <!-- Sizes your content based upon application components -->
     <v-content>
-
-        <!-- Provides the application the proper gutter -->
-        <v-container fluid>
-
-            <!-- If using vue-router -->
-            <router-view></router-view>
-        </v-container>
+      <!-- Provides the application the proper gutter -->
+      <v-container fluid>
+        <!-- If using vue-router -->
+        <router-view></router-view>
+      </v-container>
     </v-content>
 
     <v-footer app padless color="grey">
-        <v-col
-        class="text-center"
-        cols="12"
-      >
-            <v-card-text class="py-2 white--text text-center" >
-                {{ new Date().getFullYear() }} — <strong>Vuetify</strong>
-            </v-card-text>
-        </v-col>
+      <v-col class="text-center" cols="12">
+        <v-card-text class="py-2 white--text text-center">
+          {{ new Date().getFullYear() }} —
+          <strong>Vuetify</strong>
+        </v-card-text>
+      </v-col>
+      <cookie-law>
+        <div slot-scope="props">
+          <button class="skew" @click="props.accept">
+            <span>I accept</span>
+          </button>
+          <p>This site uses 🍪</p>
+          <button class="skew" @click="props.close">
+            <span>Ignore me</span>
+          </button>
+        </div>
+      </cookie-law>
     </v-footer>
-</v-app>
+  </v-app>
 </template>
 
 <script>
 import * as firebase from "firebase/app";
 import "firebase/auth";
+import CookieLaw from 'vue-cookie-law'
+
 export default {
-    name: 'App',
-    components: {},
-    async mounted() {
-        await firebase.auth().onAuthStateChanged((user) => {
-            if (user) {
-                this.user = true;
-            } else {
-                this.user = false;
-            }
-        })
-    },
-    data() {
-        return {
-            user: true,
-        }
-    },
-    methods: {
-        logout() {
-            firebase.auth().signOut()
-        },
+  name: "App",
+  components: {CookieLaw},
+  async mounted() {
+    await firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        this.user = true;
+      } else {
+        this.user = false;
+      }
+    });
+  },
+  data() {
+    return {
+      user: true
+    };
+  },
+  methods: {
+    logout() {
+      firebase.auth().signOut();
     }
+  }
 };
 </script>
