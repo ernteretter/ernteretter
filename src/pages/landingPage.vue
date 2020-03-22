5ac412e20971a5f490db7680c4ce7467c978e5d0
iBims1JFK <jonathan.kinzel@googlemail.com>
2020-03-22 19:58:04 +0100

    first

--- src/pages/landingPage.vue
+++ src/pages/landingPage.vue
@@ -1,15 +1,27 @@
 <template>
-  <div>
-    ohla
-  </div>
+<v-card class="mx-auto" max-width="800">
+    <v-card-title> Auch in dierser besondern Zeit wachsen unsere Pflanzen und versorgen uns alle mit Nahrung</v-card-title>
+    <v-card-subtitle>ein hoffnungsvolles Zeichen in der Corona-Krise. Erntehelfer, die größtenteils aus dem Ausland kommen, können aufgrund geschlossener Landesgrenzen nicht zu uns gelangen.
+        Lasst uns alle mithelfen, damit die Aussaat, Pföege und Ernte unserer Feldfrüchte auch in motivierte Erntehelfer finden und sich mit ihnen in Verbindung setzten.
+    </v-card-subtitle>
+    <v-container>
+        <v-row class="justify-center">
+            <v-btn-group>
+                <v-btn>Ich brauche Hilfe</v-btn>
+                <v-btn>Ich möchte helfen</v-btn>
+            </v-btn-group>
+        </v-row>
+    </v-container>
+</v-card>
 </template>
 
 <script>
 export default {
-  name: 'landingPage',
+    name: 'landingPage',
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
+
 <style scoped>
 </style>
