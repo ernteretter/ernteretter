ac53410d1092f2cc2c269c87cd14bd84e8a0900d
iBims1JFK <jonathan.kinzel@googlemail.com>
2020-03-23 21:55:14 +0100

    update app

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,36 @@
 <template>
 <v-app>
-    <v-app-bar app>
+    <v-navigation-drawer app disable-resize-watcher v-model="displayDrawer" hide-overlay>
+        <v-list>
+                <v-list-item>
+
+                </v-list-item>
+        </v-list>
+    </v-navigation-drawer>
+    <v-app-bar app :dense="drawer" v-resize="onResize">
         <v-row align="center" justify="center">
             <v-col>
                 <v-row>
                     <v-icon>mdi-barley</v-icon>
-                    <v-btn small outlined color=primary to="/offers" min-width="115">
+                    <v-icon color="primary" @click="displayDrawer = !displayDrawer" v-if="drawer">mdi-menu</v-icon>
+                    <v-btn v-if="!drawer" small outlined color=primary to="/offers" min-width="115">
                         Anzeigen
                         <v-spacer />
                         <v-icon small> mdi-view-dashboard-outline </v-icon>
                     </v-btn>
-                    <v-btn small outlined color=primary to="/history" v-if="user" min-width="110">
+                    <v-btn small outlined color=primary to="/history" v-if="user && !drawer" min-width="110">
                         Termine
                         <v-spacer />
                         <v-icon small> mdi-calendar-range </v-icon>
                     </v-btn>
-                    <v-btn small outlined color=primary to="/settings" v-if="user" min-width="155">
+                    <v-btn small outlined color=primary to="/settings" v-if="user && !drawer" min-width="155">
                         Einstellungen
                         <v-spacer />
                         <v-icon small> mdi-cog-outline </v-icon>
                     </v-btn>
                 </v-row>
             </v-col>
-            <v-img :src="require('../Ernteretter-Logo_03.png')" contain width="5" height="50" @click="$router.push('/')" id="bild"/>
+            <v-img :src="require('../Ernteretter-Logo_03.png')" contain width="5" height="50" @click="$router.push('/')" id="bild" />
             <v-col>
                 <!-- </v-col>
           <v-col>-->
@@ -42,7 +50,7 @@
         </v-container>
     </v-content>
 
-    <v-footer app padless color=dunkelgrau>
+    <v-footer app fixed="false" padless color=dunkelgrau>
         <v-col class="text-center" cols="12">
             <v-card-text class="py-2 white--text text-center"> Datenschutz | Impressum </v-card-text>
             <v-card-text class="py-2 white--text text-center">
@@ -80,6 +88,7 @@ export default {
         CookieLaw
     },
     async mounted() {
+        this.onResize()
         await firebase.auth().onAuthStateChanged(user => {
             if (user) {
                 this.user = true;
@@ -90,24 +99,33 @@ export default {
     },
     data() {
         return {
+            drawer: false,
+            displayDrawer: false,
             user: true
         };
     },
     methods: {
         logout() {
             firebase.auth().signOut();
-        }
+        },
+        onResize(){
+            if(window.innerWidth < 960){
+                this.drawer = true
+            } else {
+                this.drawer = false
+            }
+        },
     }
 };
 </script>
 
 <style>
 #bild {
-  cursor: pointer;
+    cursor: pointer;
 }
 
 .header123 {
-  display: initial;
+    display: initial;
 }
 
 .rounded-button-right {
