585b4e97359cf06ab2a8237c8021e474991b6f29
remunds <raban.emunds@gmail.com>
2020-03-27 22:14:19 +0100

    progress

--- functions/index.js
+++ functions/index.js
@@ -13,35 +13,61 @@ exports.deleteProfile = functions.region('europe-west2').auth.user().onDelete((u
     console.log("somebody deleted their account");
     //testing if user is helper
     admin.firestore().collection('helper').doc(user.uid).get().then((doc) => {
-    console.log("its a helper");
         //helper
         if (doc.exists) {
-            admin.firestore().collection('helper').doc.delete().then(() => {
-                console.log("Deleted helper with id: " + user.uid);
-                admin.firestore().collection('acceptedOffers').where('helperId', '==', user.uid).delete().then(() => {
-                    console.log("Deleted acceptedOffer with ")
-                    return "hi";
+            console.log("its a helper");
+            admin.firestore().collection('helper').doc().delete().then(() => {
+                console.log("deleted helper with id: " + user.uid);
+                let acceptedOffers = [];
+                admin.firestore().collection('acceptedOffers').where('helperId', '==', user.uid).get().then(snapshot => {
+                    snapshot.forEach(s => {
+                        acceptedOffers.push(s);
+                        console.log("found accepted offer form helperId. accept-offer-id: " + s.id);
+                    });
+                    console.log("found acceptedoffer from helper");
+                    return "lul";
                 }).catch(error => {
-                    console.log("error deleting acceptedOffers: " + error);
+                    console.log("error with getting acceptedOffers: " + error);
                 });
-                return "du hurensohn";
+
+                for (const offer in acceptedOffers) {
+                    admin.firestore().collection('acceptedOffers').doc(offer.id).delete().then(() => {
+                        console.log("Deleted acceptedOffers from helper: " + user.uid);
+                        return "du hurensohn";
+                    }).catch(error => {
+                        console.log("error helper delete from acceptedoffer: " + error);
+                    });
+                }
+                return "lul";
             }).catch(error => {
-                console.log("error deleting helper: " + error);
+                console.log("error deleting helper error: " + error);
             });
-            return "du hurensohn";
         }
         //not helper --> probably agrarian
         else {
             console.log("its a farmer");
-            admin.firestore().collection('agrarian').doc.delete().then(() => {
+            admin.firestore().collection('agrarian').doc().delete().then(() => {
                 console.log("User successfully deleted: " + user.uid);
-                admin.firestore.collection('offers').where('agrarianId', '==', user.id).delete().then(() => {
-                    console.log("Deleted offers from agrarian: " + user.uid);
-                    return "du hurensohn";
+                let offersWithId = [];
+                admin.firestore().collection('offers').where('agrarianId', '==', user.uid).get().then(snapshot => {
+                    snapshot.forEach(s => {
+                        offersWithId.push(s);
+                        console.log("found offer from agrarian: " + s.id);
+                    })
+                    return "lul";
                 }).catch(error => {
-                    console.log("error offer from agrarian: " + error);
+                    console.log("error with getting offers from agrarian: " + error);
                 });
-                return "du hurensohn";
+
+                for (const offer in offersWithId) {
+                    admin.firestore().collection('offers').doc(offer.id).delete().then(() => {
+                        console.log("Deleted offers from agrarian: " + user.uid);
+                        return "du hurensohn";
+                    }).catch(error => {
+                        console.log("error agrarian delete from offer: " + error);
+                    });
+                }
+                return "lul";
             }).catch(error => {
                 console.log("error deleting user: " + error);
             })
